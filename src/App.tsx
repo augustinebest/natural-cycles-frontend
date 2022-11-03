2fce8b7596b236609d9b3af246eb6bc7ccf97023
augustinebest <augustusbest00@gmail.com>
2022-11-03 13:17:12 +0100

    Imported Routing

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Router from './routes'
+import './App.css'
+import 'react-toastify/dist/ReactToastify.css';
+import 'react-phone-input-2/lib/style.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router />
+    </div>
+  )
+}
+
+export default App
