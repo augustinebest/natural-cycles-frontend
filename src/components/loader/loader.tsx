18cafe4ae87be89389f4d9612432b095808c0987
augustinebest <augustusbest00@gmail.com>
2022-11-03 14:59:48 +0100

    Created Loader component

---
+++ src/components/loader/loader.tsx
@@ -0,0 +1,20 @@
+import './loader.css'
+
+const Loader = () => {
+  return (
+    <div className="loader-frame">
+      <div className="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
