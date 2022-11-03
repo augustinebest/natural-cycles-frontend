71c5338cc367cba6af5c539b4c1fc130ef33010d
augustinebest <augustusbest00@gmail.com>
2022-11-03 22:23:13 +0100

    changed baseURL

--- src/api/requesstProcesssor.ts
+++ src/api/requesstProcesssor.ts
@@ -1,9 +1,6 @@
 import axios from 'axios'
 
-const baseURL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://natural-cycles-app.herokuapp.com'
-    : 'http://localhost:7210'
+const baseURL = 'https://natural-cycles-app.herokuapp.com'
 axios.defaults.baseURL = `${baseURL}/api/user`
 
 class ProcessRequest {
